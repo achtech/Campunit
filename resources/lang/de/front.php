e2f4fe684c3ca8f2a443bf41504d6f205b2a9005
Brahim Barjali <brahim.barjali@gmail.com>
2020-10-23 17:05:15 +0100

    feature/rent_out_camper_index

---
+++ resources/lang/de/front.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+     */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    /*
+    |-----------------Menu ------------------
+
+     */
+    'menu_home' => 'Home',
+    'menu_take-lease' => 'Take on lease',
+    'menu_insert_vehicule' => 'Rent out',
+    'menu_magazine' => 'Magazine',
+    'menu_login' => 'Login',
+    'menu_rent_motorhome' => 'Rent a motorhome',
+    'menu_rent_campervan' => 'Rent a campervan',
+    'menu_rent_caravan' => 'Rent a caravan',
+    'menu_rent_other' => 'Rent a other',
+    'menu_register' => 'Register',
+    'menu_rent' => 'Rent',
+    'menu_user_panel' => 'User Panel',
+    'menu_panel_camper' =>'My Campers',
+    'menu_panel_message' =>'Messages',
+    'menu_panel_notification' =>'Notifications',
+    'menu_panel_booking' => 'Bookings',
+    'menu_panel_wallet' =>'Wallet',
+    'menu_panel_review' =>'Reviews',
+    'menu_panel_profil' =>'My profil',
+    'menu_panel_logout' =>'Logout',
+    'menu_panel_invoice' =>'Invoice',
+    'menu_panel_dashboard_navigator'=>'Dashboard Navigation',
+    'menu_panel_account'=>'My Account',
+    /*
+    -----------------------Slider ----------
+     */
+    'slider_title' => 'Rent motorhomes, campervans and caravans from private owners',
+    'slider_looking_for' => 'What are you looking for?',
+    'slider_location' => 'Location',
+    'slider_categories' => 'All Categories',
+    'slider_search' => 'Search',
+    'Rent out your vehicle or your equipment and insurance is included' => 'Rent out your vehicle or your equipment and insurance is included',
+    'Rent out Camper' => 'Rent out Camper',
+
+    /*
+    -----------------------Footer ----------
+     */
+    'footer_title_presentation' => 'What is Campunite?',
+    'footer_presentation' => 'Campunite is the platform for renting and renting mobile homes for individuals. We bring people together! Just rent a motorhome or rent your own today.',
+    'footer_social' => 'Social',
+    'footer_paiement_methods' => 'Payment methods',
+    'footer_helpful_links' => 'Helpful Links',
+    'footer_Help' => 'Help',
+    'footer_Contact' => 'Contact',
+    'footer_Conditions' => 'Conditions',
+    'footer_Disclaimer' => 'Disclaimer',
+    'footer_imprint' => 'imprint',
+    'footer_campunit_title' => 'Campunit',
+    'footer_reserved_right' => 'All Rights Reserved',
+
+    /*
+    ---------------------- Home ------------------
+     */
+    'content_most_visited' => 'Most Visited Campers',
+    'title_categories' => 'the right one for camping',
+    'content_user_review' => 'This is how it goes on vacation',
+    'content_text_find_place' => 'Just look for the place where you would like to pick up your camper. You will be shown the nearest campers in this area and you can easily choose one of your choice.',
+    'choose_data_request' => 'If you have selected a camper, you will see all further details on the overview. Does the camper suit you? Then choose your preferred date and send the request to the landlord.',
+    'receive_request' => 'Now you just need a little patience. As soon as the landlord has received your request, he can confirm it and nothing stands in the way of your camping holiday. Have fun with your adventure.',
+    'our_bloc' => 'From The Blog',
+
+    /*
+    ---------------------- Rent out Camper page ------------------
+     */
+    'title_1' => 'Earn money with your camper',
+    'text_content_1_1' => 'Share your camper with people from all over the world. Campunite makes it as easy as possible for you. We offer simple means of payment such as PayPal, Visa and Mastercard. Read the steps and get started!',
+    'text_content_1_2' => 'Greetings to your Campunite team',
+    'learn_more' => 'Leanr more',
+    'title_2' => 'Create an account',
+    'text_content_2_1' => 'First you need a camper. You have a? Great then go ...',
+    'text_content_2_2' => 'Now create your account to get started. Fill in the details and start sharing your camper.',
+    'title_3' => 'Upload your camper',
+    'text_content_3' => "When you've created your account, you can start creating an entry for your camper. Let people know where to find your camper, show you some photos, and set a price.",
+    'title_4' => 'Done!',
+    'text_content_4' => 'Now all you have to do is wait. If someone is interested in serving campers, you will be notified and you can clarify details via the chat function if you wish.',
+    '' => '',
+
+];
