e2bfebdc1e7a6e14eb76dd049eed36ec28ead682
Brahim Barjali <brahim.barjali@gmail.com>
2020-10-23 17:38:15 +0100

    master

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -45,7 +45,7 @@
 		<!-- Copyright -->
 		<div class="row">
 			<div class="col-md-12">
-				<div class="copyrights">© 2020 {{trans('front.footer_campunit_title')}}. {{trans('front.footer_reserved_right')}}.</div>
+				<div class="copyrights">© 2020 Campunite - Made with <img style="margin-bottom: 3px;" src="{{ asset('images/general/heart.png')}}"> in Switzerland. <img style="margin-bottom: 2px;" src="{{ asset('images/general/suiss1.png')}}"></div>
 			</div>
 		</div>
 
